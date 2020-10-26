d2cf05f9f9a34c62e60ea654c9aacfdbe2d461d9
panghu666 <458787863@qq.com>
2020-10-26 22:00:58 +0800

    finish1

--- demo/try.js
+++
@@ -1,9 +0,0 @@
-// promise  书写异步代码的另一种方式    解决关于调函数嵌套问题    eg 依次读取 abcd四个文件
-
-// 创建   var p = new promise((resolve,rejected)=>{   里面一般有一个异步操作    成功调用 resolve 失败调用 rejected })
-
-// 使用  p.then(回调 resolve 改变状态为fulfilled时 调用)代表处理成功 .  上面resolve改变了状态 并且会执行成功函数 [[PromiseResult]]: 函数处理结果
-// 然后在执行回调   res 把结果拿出来
-//             catch(回调 reject 改变状态为rejected时调用) 代表处理失败    同理
-
-// promise  有三个 状态  pending    fulfilled   rejected             一旦promise的状态发生变化, 状态就会被凝固
--- src/router/index.js
+++ src/router/index.js
@@ -1,51 +1,40 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// index要写成小写  这样写了login就会自动跳转到里面的index.vue
-// @ 就直接代表src目录
-import layout from '@/views/layout'
-import Articles from '@/views/articles'
-import AddArticles from '@/views/articles/add-articles'
-import EditArticles from '@/views/articles/edit-articles'
-import Home from '@/views/home'
-import Pictures from '@/views/pictures'
-import Comment from '@/views/comment'
-import Fans from '@/views/fans'
-// import Settings from '@/views/settings'
-import Settings from '@/views/settings'
-import Page404 from '@/views/Page404'
-import login from '@/views/login'
+
 import { getUser } from '@/utils/storage.js'
+// // index要写成小写  这样写了login就会自动跳转到里面的index.vue
+// // @ 就直接代表src目录
+// import layout from '@/views/layout'
+// import Articles from '@/views/articles'
+// import AddArticles from '@/views/articles/add-articles'
+// import EditArticles from '@/views/articles/edit-articles'
+// import Home from '@/views/home'
+// import Pictures from '@/views/pictures'
+// import Comment from '@/views/comment'
+// import Fans from '@/views/fans'
+// import Settings from '@/views/settings'
+// import Page404 from '@/views/Page404'
+// import login from '@/views/login'
+
+const Login = () => import('@/views/login')
+const Layout = () => import('@/views/layout')
+const Articles = () => import('@/views/articles')
+const Comment = () => import('@/views/comment')
+const Fans = () => import('@/views/fans')
+const EditArticles = () => import('@/views/articles/edit-articles')
+const AddArticles = () => import('@/views/articles/add-articles')
+const Pictures = () => import('@/views/pictures')
+const Settings = () => import('@/views/settings')
+const Home = () => import('@/views/home')
+const Page404 = () => import('@/views/Page404')
+
 Vue.use(VueRouter)
 
-console.log(Settings, 'ss')
-
-// const router = new VueRouter({
-//   routes: [
-//     {
-//       path: '/',
-//       component: layout,
-//       children: [
-//         { path: '', component: home },
-//         { path: '/articles', component: articles }, // 文章列表
-//         { path: '/add-article', component: AddArticles },
-//         { path: '/edit-article/:id', component: EditArticles },
-//         { path: '/pictures', component: pictures },
-//         { path: '/comment', component: comment },
-//         { path: '/fans', component: fans },
-//         { path: '/settings', component: Settings }
-//       ]
-//     },
-//     { path: '/login', component: login },
-//     // 404也是一级路由, 整个页面, 都换成404
-//     // 一般在路由的配置最后, path: '*' 可以处理没被处理其他路由情况, 一般用于实现404
-//     { path: '*', component: Page404 }
-//   ]
-// })
 const router = new VueRouter({
   routes: [
     {
       path: '/',
-      component: layout,
+      component: Layout,
       children: [
         { path: '', component: Home },
         { path: '/articles', component: Articles },
@@ -57,7 +46,9 @@ const router = new VueRouter({
         { path: '/settings', component: Settings }
       ]
     },
-    { path: '/login', component: login },
+    { path: '/login', component: Login },
+    // 404也是一级路由, 整个页面, 都换成404
+    // 一般在路由的配置最后, path: '*' 可以处理没被处理其他路由情况, 一般用于实现404
     { path: '*', component: Page404 }
   ]
 })
